7f3d64def08fcc6e5ed145f51477920ea5a5ca3b
Steve Jacobs <sjacobs@truefit.io>
2023-07-10 14:51:11 -0400

    add flash

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,6 +18,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.util.project" },
+    { import = "lazyvim.plugins.extras.editor.flash" },
     { import = "lazyvim.plugins.extras.vscode" },
     -- import/override with your plugins
     { import = "plugins" },
